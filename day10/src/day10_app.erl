b580df913b884aac9501b245751afece7a6dc6c4
Pablo Meier <pablo.a.meier@gmail.com>
2019-12-11 23:34:37 -0500

    day10

--- day10/src/day10_app.erl
+++ day10/src/day10_app.erl
@@ -1,22 +1,95 @@
 -module(day10_app).
 -export([start/0]).
 
+-record(comparison, {
+    point,
+    bearing,
+    distance
+}).
+
 start() ->
     Lines = read_input_file("input.txt"),
     Asteroids = lines_to_asteroid_list(Lines),
-    VisibilityMap = line_of_sight_map_for_asteriods(Asteroids),
+    AsteroidSet = sets:from_list(Asteroids),
+
+    Pairs = pairs_of_asteroids(Asteroids),
+    IntersectionsPid = spawn(fun() -> intersections_holder(maps:new()) end),
+    parallel_map(fun ({P1, P2}) ->
+        TheRest = sets:to_list(sets:del_element(P1, sets:del_element(P2, AsteroidSet))),
+        HasBlockers = lists:any(fun (C) -> is_between(P1,C,P2) end, TheRest),
+        IntersectionsPid ! {set_can_see, P1, P2, not HasBlockers}
+    end, Pairs),
+
+    VisibilityMap = parallel_map(fun (X) ->
+        TheRest = sets:del_element(X, AsteroidSet),
+        SeeList = lists:map(fun (Y) ->
+            IntersectionsPid ! {get_can_see, X, Y, self()},
+            receive
+                {ok, true} -> 1;
+                {ok, false} -> 0
+            end
+        end, sets:to_list(TheRest)),
+        {X, lists:sum(SeeList)}
+    end, Asteroids),
+    IntersectionsPid ! kill,
     [MostVisible|_] = lists:sort(fun max_asteroids/2, VisibilityMap),
     {TheMostVisible, NumVisible} = MostVisible,
-    io:format("~nPart 1: ~p~n", [NumVisible]),
+    io:format("~nBest visibility asteroid is ~p~n", [TheMostVisible]),
+    io:format("Part 1: ~p~n", [NumVisible]),
 
-    Remaining = Asteroids -- [TheMostVisible],
-    io:format("Calculating bearings"),
-    WithBearings = [{X, bearing_between(TheMostVisible, X)} || X <- Remaining],
-    io:format(" ~p~n", [WithBearings]),
-    SortedByBearing = lists:sort(fun min_bearings/2, WithBearings),
-    io:format(" ~p~n", [SortedByBearing]),
-    {X, Y} = laser_cut(SortedByBearing, 200),
-    io:format("~nPart 2: ~p~n", [(X * 100) + Y]).
+    Remaining = sets:to_list(sets:del_element(TheMostVisible, AsteroidSet)),
+    WithBearings = [#comparison{
+                       point=X,
+                       bearing=bearing_between(TheMostVisible, X),
+                       distance=distance(TheMostVisible, X)} || X <- Remaining],
+    FauxMultimap = faux_multimap(WithBearings),
+    SortedByBearing = lists:sort(fun min_bearings/2, FauxMultimap),
+    #comparison{point={X, Y}} = laser_cut(SortedByBearing, 200),
+    io:format("Part 2: ~p~n", [(X * 100) + Y]).
+
+
+%% Bearing is your key, list of {element, distance{} are the value.
+faux_multimap(Comparisons) ->
+    AsMap = lists:foldl(fun (C = #comparison{bearing=B}, Map) ->
+        case maps:get(B, Map, not_yet) of
+            not_yet -> maps:put(B, [C], Map);
+            Lst -> maps:put(B, [C|Lst], Map)
+        end
+    end, maps:new(), Comparisons),
+    maps:to_list(maps:map(fun (_, V) -> lists:sort(fun sort_by_distance/2, V) end, AsMap)).
+
+
+sort_by_distance(#comparison{distance=D1}, #comparison{distance=D2}) ->
+    D1 =< D2.
+
+
+intersections_holder(Map) ->
+    receive
+        {set_can_see, P1, P2, Value} ->
+            intersections_holder(maps:put({P1, P2}, Value, Map));
+        {get_can_see, P1, P2, Pid} ->
+            Val = case maps:get({P1, P2}, Map, otherside) of
+                otherside -> maps:get({P2, P1}, Map);
+                X -> X
+            end,
+            Pid ! {ok, Val},
+            intersections_holder(Map);
+        kill -> ok
+    end.
+
+
+pairs_of_asteroids(List) ->
+    Initial = [{X1, X2} || X1 <- List, X2 <- List, X1 /= X2],
+    sets:to_list(lists:foldl(fun ({A, B}, Set) ->
+            case sets:is_element({B, A}, Set) of
+                true -> Set;
+                false -> sets:add_element({A, B}, Set)
+            end
+        end, sets:new(), Initial)).
+
+
+%% Reversing the ordering function since we want max on the left.
+max_asteroids({_, NumVisible1}, {_, NumVisible2}) -> NumVisible2 =< NumVisible1.
 
 
 read_input_file(Filename) ->
@@ -24,9 +97,6 @@ read_input_file(Filename) ->
     Split = binary:split(Contents, [<<",">>, <<"\n">>], [global]),
     ExtraDelimitersOut = lists:filter(fun (X) -> X =/= <<"">> end, Split),
     lists:map(fun(Line) -> binary_to_list(Line) end, ExtraDelimitersOut).
-
-%% Reversing the ordering function since we want max on the left.
-max_asteroids({_, NumVisible1}, {_, NumVisible2}) -> NumVisible2 =< NumVisible1.
 
 %% Return a set of the points as {row, col}
 lines_to_asteroid_list(L) -> lines_to_asteroid_list_accum(L, 0, []).
@@ -36,10 +106,9 @@ lines_to_asteroid_list_accum([H|T], Row, Accum) ->
     LineList = parse_line(H, Row, 0, []),
     lines_to_asteroid_list_accum(T, Row + 1, [LineList|Accum]).
 
-
 parse_line([], _, _, Accum) -> Accum;
 parse_line([$.|T], Row, Col, Accum) -> parse_line(T, Row, Col + 1, Accum);
-parse_line([$#|T], Row, Col, Accum) -> parse_line(T, Row, Col + 1, [{Row, Col}|Accum]).
+parse_line([$#|T], Row, Col, Accum) -> parse_line(T, Row, Col + 1, [{Col, Row}|Accum]).
 
 
 parallel_map(Fun, Lst) ->
@@ -55,25 +124,6 @@ receive_all(Accum, Total, SoFar) ->
     end.
 
 
-%% For every asteroid in the list, how many others are in "line of sight"? Performing super
-%% naively, since parallelism. This is closure abuse.
-line_of_sight_map_for_asteriods(AsteroidList) ->
-    parallel_map(fun (Asteroid) ->
-            WithoutMe = sets:del_element(Asteroid, sets:from_list(AsteroidList)),
-            LineOfSights = lists:filter(fun (X) ->
-                    WithoutUs = sets:del_element(X, sets:del_element(X, WithoutMe)),
-                    has_line_of_sight(X, Asteroid, sets:to_list(WithoutUs))
-                end, sets:to_list(WithoutMe)),
-
-            {Asteroid, length(LineOfSights)}
-    end, AsteroidList).
-
-
-has_line_of_sight(A, B, TheRest) ->
-    HasBlockers = lists:any(fun (C) -> is_between(A,C,B) end, TheRest),
-    not HasBlockers.
-
-
 is_between(A, C, B) -> within_epsilon(distance(A,C) + distance(C,B), distance(A, B)).
 
 distance({AY, AX},{BY, BX}) -> math:sqrt(((AX - BX) * (AX - BX)) + ((AY - BY) * (AY - BY))).
@@ -86,46 +136,30 @@ within_epsilon(A, B) -> abs(A - B) < 0.00000025.
 
 % Thanks SO https://math.stackexchange.com/questions/1596513/find-the-bearing-angle-between-two-points-in-a-2d-space
 bearing_between({AX, AY}, {BX, BY}) ->
-    ThetaPrime = math:atan2(BX - AX, BY - AY),
-    _ReturnRadians = case ThetaPrime > 0 of
+    ThetaPrime = math:atan2(BX - AX, AY - BY),
+    _ReturnRadians = case ThetaPrime >= 0 of
         true -> ThetaPrime;
         false -> (math:pi() * 2) + ThetaPrime
     end.
-    % ,
-    % %% Convert to degrees
-    % ReturnRadians * 57.2957795130823209.
 
-
-min_bearings({_, Bearing1}, {_, Bearing2}) -> Bearing1 =< Bearing2.
+min_bearings({Bearing1, _}, {Bearing2, _}) -> Bearing1 =< Bearing2.
 
 %% Run through the list, returning after you've hit Target.
 laser_cut(Remaining, Target) ->
     laser_cut_recursion(Remaining, [], Target, 1).
 
 
-laser_cut_recursion([], TheRest, Target, Curr) -> io:format("resetting! ~p ~p ~p~n", [TheRest, Target, Curr]), laser_cut_recursion(lists:reverse(TheRest), [], Target, Curr);
-laser_cut_recursion([{A,_}], _, Target, Target) -> A;
+laser_cut_recursion([], TheRest, Target, Curr) ->
+    laser_cut_recursion(lists:reverse(TheRest), [], Target, Curr);
 
 %% Cut the first in a chain: get a new tail, cons the rest for a subsequent cycle.
-laser_cut_recursion(Lst = [{_, Bearing1}|[{_, Bearing1}|_]], TheRest, Target, Curr) ->
-    case Target =:= Curr of
-        true -> io:format("okay lol were here"), erlang:halt();
-        false -> ok
+laser_cut_recursion([Entry|Tail], TheRest, Target, Curr) ->
+    {B, [H|T2]} = Entry,
+    NewTheRest = case T2 of
+        [] -> TheRest;
+        _ -> [{B, T2}|TheRest]
     end,
-    {NewTail, NewTheRest} = zoom_while_equal(Lst, TheRest),
-    laser_cut_recursion(NewTail, NewTheRest, Target, Curr + 1);
-
-laser_cut_recursion([_|T], TheRest, Target, Curr) ->
     case Target =:= Curr of
-        true -> io:format("okay lol were here"), erlang:halt();
-        false -> ok
-    end,
-    laser_cut_recursion(T, TheRest, Target, Curr + 1).
-
-
-zoom_while_equal([H|T], TheRest) ->
-    zoom_while_equal_recursion(H, T, TheRest).
-
-zoom_while_equal_recursion({Base, B1}, [{Point, B1}|T], TheRest) ->
-    zoom_while_equal_recursion({Base, B1}, T, [{Point, B1}|TheRest]);
-zoom_while_equal_recursion(_, NonMatchTail, TheRest) -> {NonMatchTail, TheRest}.
+        true -> H;
+        false -> laser_cut_recursion(Tail, NewTheRest, Target, Curr + 1)
+    end.
